74262155c58bfd7e72e051c7ff084dc131b1a698
samuel.useche <samueluseche87@gmail.com>
2024-11-05 18:09:49 -0500

    deploy 3

---
+++ src/components/InventoryIngredients.tsx
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+interface Ingredients {
+  [ingredient: string]: number;
+}
+
+const InventoryIngredients = () => {
+  const [ingredients, setIngredients] = useState<Ingredients>({}); 
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchIngredients = async () => {
+      try {
+        const token = localStorage.getItem('token');
+        if (!token) {
+          throw new Error('Token not found');
+        }
+
+        const response = await axios.get('https://30ghaa3wj8.execute-api.us-east-1.amazonaws.com/dev/inventory', {
+          headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'      
+          },
+        });
+        setIngredients(response.data); // Assuming response.data is an object of type Ingredients
+      } catch (err) {
+        if (err instanceof Error) {
+          setError(err.message);
+        } else {
+          setError('An unknown error occurred');
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchIngredients();
+  }, []);
+
+  if (loading) return <p>Cargando inventario de ingredientes...</p>;
+  if (error) return <p>Error: {error}</p>;
+
+  return (
+    <div>
+      {Object.keys(ingredients).length === 0 ? (
+        <p>No hay Ingredientes.</p>
+      ) : (
+        <table className="table table-striped table-bordered">
+          <thead>
+            <tr>
+              <th>Ingrediente</th>
+              <th>Stock</th>
+            </tr>
+          </thead>
+          <tbody>
+            {Object.entries(ingredients).map(([ingredient, quantity]) => (
+              <tr key={ingredient}>
+                <td>{ingredient}</td>
+                <td>{quantity}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
+};
+
+export default InventoryIngredients;
+
--- src/pages/IngredientInventory.tsx
+++ src/pages/IngredientInventory.tsx
@@ -1,8 +1,15 @@
 // src/pages/IngredientInventory.tsx
 import React from 'react';
+import InventoryIngredients from '../components/InventoryIngredients';
 
 const IngredientInventory: React.FC = () => {
-  return <h2>Inventario de Ingredientes</h2>;
+  
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', minHeight: '100vh' }}>
+      <h2>Inventario de Ingredientes</h2>
+      <InventoryIngredients />
+    </div>
+  );
 };
 
 export default IngredientInventory;
